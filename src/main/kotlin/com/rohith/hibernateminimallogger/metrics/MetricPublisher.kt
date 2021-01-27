4ee7295e80ebc8cb20b9da735a8b20ffceda0afc
Rohith Kunnath <rohith.kunnath@smava.de>
2021-01-27 14:36:04 +0100

    Clean the metric holder on publishing

--- src/main/kotlin/com/rohith/hibernateminimallogger/metrics/MetricPublisher.kt
+++ src/main/kotlin/com/rohith/hibernateminimallogger/metrics/MetricPublisher.kt
@@ -4,7 +4,9 @@ object MetricPublisher {
 
     fun queryWithMaxExecutionTime(): Map<String, Long> =
             MetricHolder.queryWithExecutionTime.toMap()
+                    .apply { MetricHolder.queryWithExecutionTime.clear() }
 
     fun queryWithMaxRows(): Map<String, Int> =
             MetricHolder.queryWithRows.toMap()
+                    .apply { MetricHolder.queryWithRows.clear() }
 }
--- src/test/kotlin/com/rohith/hibernateminimallogger/metrics/MetricPublisherTest.kt
+++ src/test/kotlin/com/rohith/hibernateminimallogger/metrics/MetricPublisherTest.kt
@@ -9,11 +9,13 @@ class MetricPublisherTest {
     fun shouldPublishMetric_whenQueryWithExecutionTimeIsPresent() {
         MetricHolder.queryWithExecutionTime["first_publish"] = 10
         assertEquals(10L, MetricPublisher.queryWithMaxExecutionTime()["first_publish"])
+        assertEquals(0, MetricPublisher.queryWithMaxExecutionTime().size)
     }
 
     @Test
     fun shouldPublishMetric_whenQueryWithRowsIsPresent() {
         MetricHolder.queryWithRows["second_publish"] = 10
         assertEquals(10, MetricPublisher.queryWithMaxRows()["second_publish"])
+        assertEquals(0, MetricPublisher.queryWithMaxRows().size)
     }
 }
